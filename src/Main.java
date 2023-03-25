3fe32876d6d0ca92bb9dea16d8606f510b622f83
Robiul Islam <robiulcse13@gmail.com>
2023-03-26 00:53:35 +0600

    Greeting method added

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,14 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         printMessage();
+        greetings();
     }
 
     public static void printMessage(){
         System.out.println("Hi,All Welcome to the java programming!!");
+    }
+
+    private static void greetings(){
+        System.out.println("Greetings All");
     }
 }
