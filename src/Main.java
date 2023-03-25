dbddc76bf1fecff852ca18726d40ffe1267dad8a
Robiul Islam <robiulcse13@gmail.com>
2023-03-26 00:24:39 +0600

    Creating print message function
    Added another print function with message

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        printMessage();
+    }
+
+    public static void printMessage(){
+        System.out.println("Hi,All Welcome to the java programming!!");
     }
 }
