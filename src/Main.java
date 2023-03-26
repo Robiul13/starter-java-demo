b9f5d2590638bfd970b29cadf5263986c4226b4a
Robiul Islam <robiulcse13@gmail.com>
2023-03-26 12:02:16 +0600

    Remove method printMessage

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        printMessage();
         greetings();
         printParameterValue("This is parameter value");
-    }
-
-    public static void printMessage(){
-        System.out.println("Hi,All Welcome to the java programming!!");
     }
 
     private static void greetings(){
